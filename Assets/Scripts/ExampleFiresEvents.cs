28cb477b7555bfe28907c9021fe67be47639b868
Peter Koch <peterept@gmail.com>
2017-04-26 20:33:27 +1000

    example with unity events

---
+++ Assets/Scripts/ExampleFiresEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExampleFiresEvents : MonoBehaviour {
+
+	public UnityEvent onSomethingHappenedEvent;
+
+	void Update () {
+
+		if (Input.GetButtonDown ("Fire1")) {
+			onSomethingHappenedEvent.Invoke ();
+		}
+	}
+}
---
+++ Assets/Scripts/ExampleReceiveEvents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleReceiveEvents : MonoBehaviour {
+
+	public void OnSomethingHappened() {
+		RandomizeMyColor ();
+	}
+
+	void RandomizeMyColor() {
+		GetComponent<Renderer>().material.color = new Color (Random.value, Random.value, Random.value, 1.0f);
+	}
+}
